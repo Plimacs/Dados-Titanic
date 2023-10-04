0f6ec5196d57cdfab4688d3d5395db54a886a1a4
Matheus Pereira Lima <132246877+Plimacs@users.noreply.github.com>
2023-10-04 12:03:59 -0300

    #4 Gráfico pizza implementado

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import statistics
+import matplotlib.pyplot as pizza
 
 # Carrega os arquivos CSV em um DataFrame.
 df = pd.read_csv('./Dados/dados4.csv')
@@ -17,8 +18,30 @@ df['age'] = df['age'].round(0).astype(int)
 # Imprime a correção dos dados em formato TXT.
 df.to_csv('./Respostas/Resposta01.txt', index=False)
 
+# Contando todos os Homens e Mulheres da coluna Sex.
 Quantidade_Homens = df['sex'].value_counts()['male']
 Quantidade_Mulheres = df['sex'].value_counts()['female']
 
-print('Existem', Quantidade_Homens, 'Homens e', Quantidade_Mulheres, 'Mulheres, somando um Total de', Quantidade_Homens + Quantidade_Mulheres, 'pessoas.')
+# Informa a quantidade de cada sexo e o total de pessoas.
+print('Existem', Quantidade_Homens, 'Homens e', Quantidade_Mulheres, 'Mulheres, somando um total de', Quantidade_Homens + Quantidade_Mulheres, 'pessoas.')
 
+# Conta o número de sobreviventes e não sobreviventes.
+contagem_sobreviventes = df['survived'].value_counts()
+
+# Determina os parâmetro da pizza: texto, tamanho, cores, a forma que as fatias 
+# são exibidas, sombras, e o ângulo.
+labels = ['Não sobreviventes', 'sobreviventes']
+sizes = [contagem_sobreviventes[0], contagem_sobreviventes[1]]
+colors = ['red', 'green']
+explode = (0.1, 0)
+autopct = '%1.1f%%'
+shadow = True
+startangle = 50
+
+# Cria um gráfico de pizza.
+pizza.pie(sizes, explode=explode, labels=labels, colors=colors, autopct=autopct, shadow=shadow, startangle=startangle)
+pizza.axis('equal')
+
+# Exibe o gráfico.
+pizza.title('Porcentagem de sobreviventes e não sobreviventes')
+pizza.show()
